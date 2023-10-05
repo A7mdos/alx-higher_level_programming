73888854d42d1774d66ef0449f82508737e97046
A7mdos <a7mdos1999@gmail.com>
2023-10-06 00:53:52 +0200

    Mandatory tasks 0 - 5

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv)
+    arguments = argc - 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arguments))
+ 
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
