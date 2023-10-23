5d85aa9883186dd905750d453ce223a0a937be72
A7mdos <a7mdos1999@gmail.com>
2023-10-23 21:10:55 +0200

    pycodestyle lay ay zooool ya

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,11 @@
 
 from sys import stderr, exc_info
 
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except:
+    except Exception:
         print("Exception: {}".format(exc_info()[1]), file=stderr)
         return None
