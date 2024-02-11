13eb9d4ce9808ca8dce3370ae5af2213df76930b
Ahmed Osman Ibrahim <77128331+A7mdos@users.noreply.github.com>
2024-02-11 12:54:07 +0200

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-if __name__ == "__main__":
-    import hidden_4
-
-    namespace = dir(hidden_4)
-    for name in namespace:
-        if name[0:2] != "__":
-            print(name)
